f41822024269ed18259873d336f914255792fe8c
Marius Toft <marius.toft@icloud.com>
2024-02-03 17:59:20 +0000

    Update server port to 80

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/", helloHandler)
 
-    fmt.Println("Starting server at port 8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        panic(err)
-    }
+	fmt.Println("Starting server at port 80")
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		panic(err)
+	}
 }
