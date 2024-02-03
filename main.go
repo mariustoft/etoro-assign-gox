b92bad8c3f69992c45b6b09b15a6b15d201324fd
Marius Toft <marius.toft@icloud.com>
2024-02-03 23:13:32 +0000

    Add HTML form with phone number input

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		templ, _ := template.New("test").Parse("Hello, {{.}}!")
-		templ.Execute(w, "World")
+		// templ, _ := template.New("test").Parse("Hello, {{.}}!")
+		// add html file
+		templ, _ := template.ParseFiles("index.html")
+
+		templ.Execute(w, "")
 
 	})
+	
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
