f9bbb2c2f5f6e9936c4dc22719f6be7d80a4f9fe
Marius Toft <marius.toft@icloud.com>
2024-02-04 09:28:53 +0000

    Update index.html and main.go files

--- main.go
+++ main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// declare variable message
+		// message := "Hello World"
+		phone := r.Form.Get("hone")
+		log.Println("phone-->", phone)
 
-		// templ, _ := template.New("test").Parse("Hello, {{.}}!")
-		// add html file
-		templ, _ := template.ParseFiles("index.html")
-
-		templ.Execute(w, "")
+		root, _ := template.ParseFiles("index.html")
+		// wallet, _ := template.ParseFiles("wallet.html")
+		// add variable to template
+		root.Execute(w, "message")
 
 	})
-	
+
+	http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		// declare variable message
+		message := "Hello World"
+		// add variable to template
+		root, _ := template.ParseFiles("index.html")
+		root.Execute(w, message)
+
+	})
+
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
