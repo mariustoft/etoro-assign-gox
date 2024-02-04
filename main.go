51207198857e57fa9abad0f52ce87478a7ebf7b3
Marius Toft <marius.toft@icloud.com>
2024-02-04 15:38:51 +0000

    Add currency conversion functionality

--- main.go
+++ main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
+	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+var portfolio = []map[string]interface{}{
+	{"Name": "cardano", "Quantity": 12},
+	{"Name": "bitcoin", "Quantity": 22},
+	{"Name": "ethereum", "Quantity": 32},
+	{"Name": "eolana", "Quantity": 52},
+}
+
+var currencies = []string{"usd", "eur", "gbp", "jpy", "cny"}
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("templates/home.html", "templates/header.html", "templates/convertor.html")
+		tmpl, _ := template.ParseFiles("templates/home.html", "templates/convertor.html")
 
 		tmpl.Execute(w, map[string]interface{}{
-			"Header": r.FormValue("new-header"),
-			"Portfolio": []map[string]interface{}{
-				{"Name": "Cardano", "Quantity": 12},
-				{"Name": "Bitcoin", "Quantity": 22},
-				{"Name": "Ethereum", "Quantity": 32},
-				{"Name": "Solana", "Quantity": 52},
-			},
-			"Currencies": []string{"USD", "EUR", "GBP", "JPY", "CNY"},
+			"currencies": currencies,
+			"portfolio":  portfolio,
 		})
 
 	})
@@ -27,15 +33,26 @@ func main() {
 	http.HandleFunc("/convertor", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, _ := template.ParseFiles("templates/convertor.html")
 
+		selectedCoin := r.FormValue("selectedCoin")
+		selectedCurrency := r.FormValue("selectedCurrency")
+		selectedAmount, _ := strconv.ParseFloat(r.FormValue("selectedAmount"), 64)
+
+		url := "https://api.coingecko.com/api/v3" + "/simple/price"
+		url += "?ids=" + selectedCoin + "&vs_currencies=" + selectedCurrency + "&amount=" + strconv.FormatFloat(selectedAmount, 'f', -1, 64)
+
+		resp, _ := http.Get(url)
+		body, _ := io.ReadAll(resp.Body)
+
+		data := make(map[string]map[string]float64)
+		json.Unmarshal([]byte(body), &data)
+
 		tmpl.Execute(w, map[string]interface{}{
-			"selectedAmount": r.FormValue("selectedAmount"),
-			"Portfolio": []map[string]interface{}{
-				{"Name": "Cardano", "Quantity": 12},
-				{"Name": "Bitcoin", "Quantity": 22},
-				{"Name": "Ethereum", "Quantity": 32},
-				{"Name": "Solana", "Quantity": 52},
-			},
-			"Currencies": []string{"USD", "EUR", "GBP", "JPY", "CNY"},
+			"selectedAmount":       selectedAmount,
+			"selectedCoin":         selectedCoin,
+			"selectedCurrency":     selectedCurrency,
+			"calculatedConversion": data[selectedCoin][selectedCurrency] * selectedAmount,
+			"currencies":           currencies,
+			"portfolio":            portfolio,
 		})
 
 	})
