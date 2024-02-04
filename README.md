be323309e75eee5909806e4d117b646931d37eec
Marius Toft <marius.toft@icloud.com>
2024-02-04 12:48:41 +0000

    Update README.md and main.go

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # etoro-assign-gox
 
-repo: https://github.com/mariustoft/etoro-assign-gox
-app: https://etoro-assign-gox.up.railway.app
+- Repository: [https://github.com/mariustoft/etoro-assign-gox](https://github.com/mariustoft/etoro-assign-gox)
+- Live Application: [https://etoro-assign-gox.up.railway.app](https://etoro-assign-gox.up.railway.app)
+
+## Running Locally
+
+To run the application locally, follow these steps:
+
+1. Install the `gin` package by running the following command:
+
+   ```shell
+   go get github.com/codegangsta/gin
+
+   ```
+
+2. Run the application using the following command:
+
+   ```shell
+   gin run main.go
+
+   ```
+
+3. The application will be available at `http://localhost:3000`.
--- main.go
+++ main.go
@@ -2,31 +2,23 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	tmpl, _ := template.ParseFiles("home.html", "header.html")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// declare variable message
-		// message := "Hello World"
-		phone := r.Form.Get("hone")
-		log.Println("phone-->", phone)
 
-		root, _ := template.ParseFiles("index.html")
-		// wallet, _ := template.ParseFiles("wallet.html")
-		// add variable to template
-		root.Execute(w, "message")
-
-	})
-
-	http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
-		// declare variable message
-		message := "Hello World"
-		// add variable to template
-		root, _ := template.ParseFiles("index.html")
-		root.Execute(w, message)
+		tmpl.Execute(w, map[string]interface{}{
+			"Header": r.FormValue("new-header"),
+			"Portfolio": []map[string]interface{}{
+				{"Name": "BTC", "Price": 10000.0},
+				{"Name": "ETH", "Price": 200.0},
+				{"Name": "XRP", "Price": 0.5},
+			},
+		})
 
 	})
 
